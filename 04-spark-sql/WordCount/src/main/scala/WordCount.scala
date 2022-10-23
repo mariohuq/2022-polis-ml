86ef25257743c6fd5e968a9588066a06be12dffa
mariohuq <15021607+mariohuq@users.noreply.github.com>
2022-10-23 20:04:06 +0300

    add stopList

--- 04-spark-sql/WordCount/src/main/scala/WordCount.scala
+++ 04-spark-sql/WordCount/src/main/scala/WordCount.scala
@@ -1,5 +1,6 @@
 package ok.ml
 
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
@@ -7,18 +8,27 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().getOrCreate()
     import spark.sqlContext.implicits._
-    def wordCount(df: DataFrame): DataFrame = df
+    def wordCount(df: DataFrame, stopWords: Broadcast[Set[String]]): DataFrame = df
       .select(concat_ws(" ", $"class", $"comment") as "text")
       .select(split($"text", """(\s|\p{Punct})+""") as "words")
       .select(explode($"words") as "word")
       .select(lower($"word") as "word")
+      .filter(row => !stopWords.value.contains(row.getString(0)))
       .groupBy($"word").count().as("count")
       .select($"count", $"word")
       .sort($"count".desc)
 
-    val Array(input, output) = args
-    wordCount(spark.read.option("header", "true").csv(input))
-      .write.mode("overwrite").csv(output)
+    val Array(input, output, stopList) = args
+    val stopWords = spark.read
+      .text(stopList)
+      .select(lower($"value"))
+      .as[String]
+      .collect()
+      .toSet
+    wordCount(
+      spark.read.option("header", "true").csv(input),
+      spark.sparkContext.broadcast(stopWords)
+    ).write.mode("overwrite").csv(output)
     spark.stop()
   }
 }

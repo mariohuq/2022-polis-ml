0e92487d69446b64cce78e823d50a777f98237c9
mariohuq <15021607+mariohuq@users.noreply.github.com>
2022-10-23 18:14:23 +0300

    impl base wc

--- 04-spark-sql/WordCount/build.sbt
+++ 04-spark-sql/WordCount/build.sbt
@@ -1,9 +1,12 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+ThisBuild / scalaVersion := "2.11.12" // since no spark core is on Maven for recent Scala version
+                                      // AND spark is running on that specific version
 
 lazy val root = (project in file("."))
   .settings(
     name := "WordCount",
     idePackagePrefix := Some("ok.ml")
   )
+
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-sql" % "2.4.8" % "provided")
--- 04-spark-sql/WordCount/src/main/scala/Main.scala
+++ 04-spark-sql/WordCount/src/main/scala/Main.scala
@@ -1,7 +1,22 @@
 package ok.ml
 
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    import spark.sqlContext.implicits._
+
+    def wordCount(df: DataFrame): DataFrame = df
+      .select(concat_ws(" ", $"class", $"comment") as "text")
+      .select(split($"text", "\\s") as "words")
+      .select(explode($"words") as "word")
+      .groupBy($"word").count()
+
+    val Array(input, output) = args
+    val spark = SparkSession.builder().getOrCreate()
+    wordCount(spark.read.option("header", "true").csv(input))
+      .write.mode("overwrite").csv(output)
+    spark.stop()
   }
 }
